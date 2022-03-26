fdc6dfdaff9c10cf469b299dba237ddef7a90306
Raúl Herrerías Corral <raulhc1313@gmail.com>
2022-03-26 01:34:03 +0100

    a

--- Kotlin_proyecto/src/Dos.kt
+++ Kotlin_proyecto/src/Dos.kt
@@ -1,12 +1,12 @@
-     fun main(){
+     
+	fun main(){
+		 
         var string:String = "Introduce una String: "
         println(string)
         var input1 = readLine()
-        println("La string introducida es: $input1 y su longitud es ")
+        println("La string introducida es: $input1 ")
         println(string)
         var input2 = readLine()
-        println("La string introducida es: $input2")
-        println("La longitud de la primera String es: $input1.length")
-        println("La longitud de la segunda String es: $input2.length")
+        println("La string introducida es: $input2 ")
     }
    
